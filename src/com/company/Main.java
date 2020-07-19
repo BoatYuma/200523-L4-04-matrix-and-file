e366923b92f409db8f75aaa6000e45fb64871d3a
BoatYuma <66005374+BoatYuma@users.noreply.github.com>
2020-07-19 16:12:20 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,31 +15,31 @@ public class Main {
                     new BufferedReader(new InputStreamReader(System.in));
 
             System.out.print("Dimension N:");
-            int dimentionN = Integer.parseInt(in_stream_char.readLine());
-            System.out.print("Dimention M:");
-            int dimentionM = Integer.parseInt(in_stream_char.readLine());
-            System.out.print("Dimention P:");
-            int dimentionP = Integer.parseInt(in_stream_char.readLine());
+            int dimensionN = Integer.parseInt(in_stream_char.readLine());
+            System.out.print("dimension M:");
+            int dimensionM = Integer.parseInt(in_stream_char.readLine());
+            System.out.print("dimension P:");
+            int dimensionP = Integer.parseInt(in_stream_char.readLine());
 
-            Double[][] matrix1 = new Double[dimentionN][dimentionM];
-            Double[][] matrix2 = new Double[dimentionM][dimentionP];
+            Double[][] matrix1 = new Double[dimensionN][dimensionM];
+            Double[][] matrix2 = new Double[dimensionM][dimensionP];
 
-            parseMatrix(in_stream_char, dimentionN, dimentionM, matrix1, MATRIX_1_PARSING_COMMENT);
-            parseMatrix(in_stream_char, dimentionM, dimentionP, matrix2, MATRIX_2_PARSING_COMMENT);
+            parseMatrix(in_stream_char, dimensionN, dimensionM, matrix1, MATRIX_1_PARSING_COMMENT);
+            parseMatrix(in_stream_char, dimensionM, dimensionP, matrix2, MATRIX_2_PARSING_COMMENT);
 
-            Double[][] matrix3 = new Double[dimentionN][dimentionP];
-            for (int i = 0; i < dimentionN; i++)
-                for (int j = 0; j < dimentionP; j++) {
+            Double[][] matrix3 = new Double[dimensionN][dimensionP];
+            for (int i = 0; i < dimensionN; i++)
+                for (int j = 0; j < dimensionP; j++) {
                     matrix3[i][j] = 0.0;
-                    for (int k = 0; k < dimentionM; k++)
+                    for (int k = 0; k < dimensionM; k++)
                         matrix3[i][j] = matrix3[i][j] + matrix1[i][k] * matrix2[k][j];
 
                 }
             PrintStream out_stream = new PrintStream(
                     new FileOutputStream("resources/out.txt"));
 
-            for (int i = 0; i < dimentionN; i++) {
-                for (int j = 0; j < dimentionP; j++)
+            for (int i = 0; i < dimensionN; i++) {
+                for (int j = 0; j < dimensionP; j++)
                     out_stream.print(matrix3[i][j] + " ");
                 out_stream.println();
             }
@@ -51,9 +51,9 @@ public class Main {
 
     }
 
-    private static void parseMatrix(BufferedReader in_stream_char, int dimentionN, int dimentionM, Double[][] matrix1, String s) throws IOException {
-        for (int i = 0; i < dimentionN; i++)
-            for (int j = 0; j < dimentionM; j++) {
+    private static void parseMatrix(BufferedReader in_stream_char, int dimensionN, int dimensionM, Double[][] matrix1, String s) throws IOException {
+        for (int i = 0; i < dimensionN; i++)
+            for (int j = 0; j < dimensionM; j++) {
                 System.out.print(s + i + j + ":");
                 matrix1[i][j] = Double.parseDouble(in_stream_char.readLine());
             }
